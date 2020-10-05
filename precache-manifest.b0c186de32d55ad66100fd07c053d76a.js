fe803642673b0a60838d03f76d4c8b5f7230b0bb
Serg <serggbobrov@gmail.com>
2020-10-05 14:43:25 +0300

    Updates

---
+++ precache-manifest.b0c186de32d55ad66100fd07c053d76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454f08fbd39886362cdcb5d0fab6dd09",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "771a3b2ffda65971423e",
+    "url": "/simple-react-homeworks-master/static/css/main.e43f256a.chunk.css"
+  },
+  {
+    "revision": "9e2a62f40b5056fc53e1",
+    "url": "/simple-react-homeworks-master/static/js/2.a133f33b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks-master/static/js/2.a133f33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771a3b2ffda65971423e",
+    "url": "/simple-react-homeworks-master/static/js/main.b0920223.chunk.js"
+  },
+  {
+    "revision": "4d3b136f42fc51341e1d",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.39865212.js"
+  }
+]);
