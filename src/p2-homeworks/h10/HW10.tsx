152cf3ea57c023fb7bd500bafb14728f8a214a56
Serg <serggbobrov@gmail.com>
2020-10-13 17:01:23 +0300

    hw8 is done

---
+++ src/p2-homeworks/h10/HW10.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+
+function HW10() {
+    // useSelector, useDispatch
+    const loading = false;
+
+    const setLoading = () => {
+        // dispatch
+        // setTimeout
+        console.log("loading...");
+    };
+
+    return (
+        <div>
+            <hr/>
+            homeworks 10
+
+            {/*should work (должно работать)*/}
+            {loading
+                ? (
+                    <div>крутилка...</div>
+                ) : (
+                    <div>
+                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
+                    </div>
+                )
+            }
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<Alternative/>*/}
+            <hr/>
+        </div>
+    );
+}
+
+export default HW10;
