781b6a7debcc1b1948538dcd2002c3c0d2d88c1c
Serg <serggbobrov@gmail.com>
2020-11-04 12:27:45 +0300

    slider

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,6 +6,15 @@ function HW11() {
     const [value1, setValue1] = useState(0);
     const [value2, setValue2] = useState(100);
 
+    const setChangeRange1 = (value: number) => {
+        setValue1(value)
+    }
+
+   const setChangeRange2 = ([value1, value2]:number[]) => {
+        setValue1(value1)
+        setValue2(value2)
+    }
+
     return (
         <div>
             <hr/>
@@ -16,13 +25,17 @@ function HW11() {
                 <span>{value1}</span>
                 <SuperRange
                     // сделать так чтоб value1 изменялось
+                    value={value1}
+                    onChangeRange = {setChangeRange1}
                 />
             </div>
-
+            <hr/>
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
+                    onChangeRange={setChangeRange2}
+                    value={[value1, value2]}
                 />
                 <span>{value2}</span>
             </div>
--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import HW11 from "../../h11/HW11";
 
 
 
 export const JuniorPlus = () => {
     return (
-        <div>JuniorPlus</div>
+        <>
+            <HW11/>
+        </>
     );
 };
